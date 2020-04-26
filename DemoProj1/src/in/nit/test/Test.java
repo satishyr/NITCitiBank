55ba56a40f655ab5fa5ffee964b13ce89df7f809
DEV1 <DEV1@gmail.com>
2020-04-26 21:07:30 +0530

    Test.java is added to Demo.java

---
+++ DemoProj1/src/in/nit/test/Test.java
@@ -0,0 +1,13 @@
+package in.nit.test;
+
+import in.nit.beans.Utility;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Utility u=new Utility();
+		System.out.println(u.getUtility());
+	}
+
+}
