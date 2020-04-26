849fa9553edfe8fe44f61ad644a5f143afc6ad71
DEV1 <DEV1@gmail.com>
2020-04-26 21:29:46 +0530

    m1() modified

--- DemoProj1/src/in/nit/test/Test.java
+++ DemoProj1/src/in/nit/test/Test.java
@@ -2,10 +2,6 @@ package in.nit.test;
 
 public class Test {
 public void m1() {
-<<<<<<< HEAD
 	System.out.println("utility m1() method.....abcd...");
-=======
-	System.out.println("utility m1() method:1234");
->>>>>>> branch 'master' of https://github.com/satishyr/NITCitiBank.git
 }
 }
